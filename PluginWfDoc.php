6d088ae99e05b49a808afcddb1bdae0b66df39e2
Claes Östlund <>
2023-08-08 17:36:23 +0200

    1.2.0

--- PluginWfDoc.php
+++ PluginWfDoc.php
@@ -27,7 +27,7 @@ class PluginWfDoc{
     if(file_exists($filename)){
       $page = sfYaml::load($filename);
       wfArray::set($GLOBALS, 'sys/filename', $filename);
-      wfArray::set($GLOBALS, 'sys/layout_path', '/theme/'.wfArray::get($GLOBALS, 'sys/theme').'/'.$settings->get('layout_folder'));
+      wfGlobals::setSys('layout_path', '/theme/'.wfArray::get($GLOBALS, 'sys/theme').'/'.$settings->get('layout_folder'));
       wfDocument::mergeLayout($page);
     }else{
       wfEvent::run('page_not_found', array('description' => 'PluginWfDoc could not find the file '.$settings->get('page_folder').'/'.$method.'.yml.', 'plugin' => wfArray::get($GLOBALS, 'sys/plugin'), 'class' => wfArray::get($GLOBALS, 'sys/class'), 'method' => $method, 'filename' => $filename));
